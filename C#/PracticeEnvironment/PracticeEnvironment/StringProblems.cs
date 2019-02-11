c5c7e06286721c82b5d100b321a9cb2ec20c78ad
Christopher Wood <cmw2379@gmail.com>
2019-02-10 23:02:23 -0500

    Added palindrome solution with tests

---
+++ C#/PracticeEnvironment/PracticeEnvironment/StringProblems.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PracticeEnvironment
+{
+    public static class StringProblems
+    {
+        //Currently does not work. Needs to work recursively since this only cover one set of the permutations
+        public static IEnumerable<string> GetPermutations(string value)
+        {
+            var stringPermutations = new List<string>();
+
+            for (var i = 0; i < value.Length; i++)
+            {
+                var rootCharacter = Convert.ToString(value[i]);
+
+                for (var j = 0; j < value.Length; j++)
+                {
+                    var swappingCharacter = Convert.ToString(value[j]);
+                    var permutation = value;
+                    permutation = permutation.Remove(i, 1);
+                    permutation = permutation.Insert(i, swappingCharacter);
+                    permutation = permutation.Remove(j, 1);
+                    permutation = permutation.Insert(j, rootCharacter);
+
+                    if (!stringPermutations.Contains(permutation))
+                        stringPermutations.Add(permutation);
+                }
+            }
+
+            return stringPermutations;
+        }
+
+        public static bool IsPalindrome(string value)
+        {
+            value = value.ToLower();
+
+            for (int head = 0, tail = value.Length - 1; head < (value.Length/2); head++, tail--)
+            {
+                if (value[head] != value[tail])
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ C#/PracticeEnvironment/PracticeEnvironmentTests.cs/StringProblemsTests.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PracticeEnvironment;
+
+namespace PracticeEnvironmentTests.cs
+{
+    [TestClass]
+    public class StringProblemsTests
+    {
+        [TestMethod]
+        public void GetPermutationsReturnsCorrectListOfPermutations()
+        {
+            var actualPermutations = StringProblems.GetPermutations("aab");
+
+            Assert.AreEqual(3, actualPermutations.Count());
+            Assert.IsTrue(actualPermutations.Contains("aab"));
+            Assert.IsTrue(actualPermutations.Contains("aba"));
+            Assert.IsTrue(actualPermutations.Contains("baa"));
+        }
+
+        [TestMethod]
+        public void GetPermutationsReturnsCorrectListOfPermutationsWithAllSeparateCharacters()
+        {
+            var actualPermutations = StringProblems.GetPermutations("abc");
+
+            Assert.AreEqual(7, actualPermutations.Count());
+        }
+
+        [TestMethod]
+        public void IsPalindromeReturnsTrueForEvenCharacterPalindrome()
+        {
+            Assert.IsTrue(StringProblems.IsPalindrome("redder"));
+        }
+
+        [TestMethod]
+        public void IsPalindromeReturnsTrueForOddCharacterPalindrome()
+        {
+            Assert.IsTrue(StringProblems.IsPalindrome("racecar"));
+        }
+
+        [TestMethod]
+        public void IsPalindromeReturnsFalseForInvalidPalindrome()
+        {
+            Assert.IsFalse(StringProblems.IsPalindrome("Notapalindrome"));
+        }
+
+        [TestMethod]
+        public void IsPalindromeIgnoresCharacterCase()
+        {
+            Assert.IsTrue(StringProblems.IsPalindrome("Racecar"));
+        }
+    }
+}
