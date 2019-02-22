dc1b42aa6fae2fd575397f6463dd2fbf86ec21ad
Christopher Wood <cwood@qualtrax.com>
2019-02-22 15:10:15 -0500

    renames and added F# FizzBuzz

---
+++ F#/BowlingGameKata/BowlingGame/FizzBuzz.fs
@@ -0,0 +1,20 @@
+﻿module FizzBuzz
+
+let numbers = [|1..100|]
+
+let fizzBuzzForNumber number =
+    match number with
+    | number when (number % 15) = 0 ->
+        "FizzBuzz"
+    | number when (number % 5) = 0 ->
+        "Buzz"
+    | number when (number % 3) = 0 ->
+        "Fizz"
+    | number ->
+        sprintf "%d" number
+    
+
+let fizzBuzzForList numbers =
+    numbers |> Array.iter (fun n -> printfn "%s" (fizzBuzzForNumber n))
+
+fizzBuzzForList numbers
